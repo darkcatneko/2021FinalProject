2c7816ed791653b7fd2c1829c6c0f356c0c556a8
DESKTOP-EUTIRR1\User <darkcat7126@gmail.com>
2021-12-21 01:36:51 +0800

    movement check

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
+    public bool m_FacingRight = true;
     public Rigidbody rb;
 
     Vector3 movement;
@@ -15,10 +15,25 @@ public class PlayerMovement : MonoBehaviour
     {
        movement.x = Input.GetAxisRaw("Horizontal");
        movement.z = Input.GetAxisRaw("Vertical");
+        if (movement.x > 0 && !m_FacingRight)
+        {
+            Flip();
+        }
+
+        else if (movement.x < 0 && m_FacingRight)
+        {
+            Flip();
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);       
+    }
+    private void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
+        float scaleX = this.transform.localScale.x;
+        this.transform.localScale = new Vector3(scaleX, this.transform.localScale.y,this.transform.localScale.z);
     }
 }
