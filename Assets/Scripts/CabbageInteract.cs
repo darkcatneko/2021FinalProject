a88f648fb7804abc52cdf22ea24f3f8118cb6c30
DESKTOP-EUTIRR1\User <darkcat7126@gmail.com>
2021-12-25 18:52:31 +0800

    Near Plant Test(success)

--- Assets/Scripts/CabbageInteract.cs
+++ Assets/Scripts/CabbageInteract.cs
@@ -6,11 +6,15 @@ using UnityEngine;
 public class CabbageInteract : MonoBehaviour
 {
     [SerializeField] GameObject fertilizeVfx_Spawnpoint;
+    [SerializeField] GameObject Fertilize2D_VFX;
     public GameObject vfx;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            this.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+            this.GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", new Color(0.1f, 0.1f, 0.1f, 0));
+            //DynamicGI.UpdateEnvironment();
             Debug.Log(other.name + "is in");
         }
     }
@@ -18,7 +22,8 @@ public class CabbageInteract : MonoBehaviour
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
-        {            
+        {
+            
             if (Input.GetKeyDown(KeyCode.F) && this.gameObject.GetComponent<PlantPerform>().GetPlantState() == 3 && other.GetComponentInParent<PlayerMovement>().movement.z <= 0)//±Ä¦¬
             {
                 Debug.Log("get a mature cabbage!");
@@ -45,6 +50,9 @@ public class CabbageInteract : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            this.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+            this.GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", new Color(0,0,0,0));
+            //DynamicGI.UpdateEnvironment();
             Debug.Log(other.name + "is out");
         }
     }
@@ -52,7 +60,14 @@ public class CabbageInteract : MonoBehaviour
     public void Fertilize()
     {
         this.GetComponent<PlantPerform>().This_Plant.Is_fertilize = true;
-        vfx = Instantiate(Resources.Load("Partical/PS_fertilize") as GameObject, fertilizeVfx_Spawnpoint.transform.position, Quaternion.identity);
+        GameObject _vfx = Instantiate(Fertilize2D_VFX, transform.position + new Vector3(0, 0.3f, 0.2f),Quaternion.Euler(45,0,0));
+        Destroy(_vfx, 1.05f);
+        StartCoroutine(Delay.DelayToInvokeDo(() =>
+        {
+            vfx = Instantiate(Resources.Load("Partical/PS_fertilize") as GameObject, fertilizeVfx_Spawnpoint.transform.position, Quaternion.identity);
+        }
+        , 0.8f));
+        
     }
 
     public void Defertilize()
